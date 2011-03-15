5378f90f47c7f62ed68bf4beb2503181839cb24a
Elmer Garduno <elmer.garduno@gmail.com>
2011-03-15 00:39:41 -0400

    Added project files

---
+++ src/main/java/mx/bigdata/anyobject/MapBasedAnyObject.java
@@ -0,0 +1,80 @@
+/*
+ *  Copyright 2011 Elmer Garduno
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package mx.bigdata.anyobject;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class MapBasedAnyObject implements AnyObject {
+
+  private final Map map;
+
+  private final String separator;
+  
+  public MapBasedAnyObject(Map map) {
+    this(map, "\\.");
+  }
+
+  public MapBasedAnyObject(Map map, String separator) {
+    this.map = map;
+    this.separator = separator;
+  }
+
+  private Object get(String key) {
+    return get(map, key.split("\\."));    
+  }
+
+  private Object get(Map inner, String[] key) {
+    Object o = inner.get(key[0]);
+    return (key.length > 1) 
+      ? get((Map) o, Arrays.copyOfRange(key, 1, key.length))
+      : o; 
+  } 
+
+  public AnyObject getAnyObject(String key) {
+    return new MapBasedAnyObject((Map) get(key));
+  }
+
+  public Long getLong(String key) {
+    return (Long) get(key);
+  }
+
+  public Integer getInteger(String key) {
+    return (Integer) get(key);
+  }
+
+  public String getString(String key) {
+    return (String) get(key);
+  }
+
+  public Double getDouble(String key) {
+    return (Double) get(key);  
+  }
+
+  public Float getFloat(String key) {
+    return (Float) get(key);    
+  }
+
+  public Iterable getIterable(String key) {
+    return (Iterable) get(key);
+  }
+
+  public String toString() {
+    return map.toString();
+  }
+
+}
---
+++ src/main/java/mx/bigdata/anyobject/impl/JacksonJSONLoader.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright 2011 Elmer Garduno
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package mx.bigdata.anyobject.impl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import mx.bigdata.anyobject.MapBasedAnyObject;
+import mx.bigdata.anyobject.AnyObject;
+
+public final class JacksonJSONLoader {
+
+  public static AnyObject load(File file) throws IOException {
+    InputStream in = new FileInputStream(file);
+    try {
+      return load(in);
+    } finally {
+      in.close();    
+    }
+  }
+
+  public static AnyObject load(InputStream in) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    Map<String, Object> map = mapper.readValue(in, Map.class);
+    return new MapBasedAnyObject(map);
+  }
+
+}
